cae4348963a64ed879651b688b87cdbef05c3717
anaraa@code.edu.az <you@example.com>
2022-12-07 16:46:50 -0800

    c#task

---
+++ CSharp-task1/Program.cs
@@ -0,0 +1,100 @@
+﻿
+
+//Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+
+
+//////n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+
+
+
+//int n = 8;
+
+//int m = 16;
+//{
+
+//    if (n % 2 == 0 && m % 2 == 0)
+
+//    {
+//        sum = n + m;
+
+
+//    }
+
+//}
+//    Console.WriteLine(sum);
+
+
+
+
+//Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin
+
+//int sum = 0;
+
+//int n = 50;
+//int m = 100;
+
+//for (int i = n; i < m; i++)
+//{
+//    if (i % 2 == 1)
+
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+//Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+
+
+//int count = 0;
+
+//int[] arr = new int[60];
+
+
+
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 0)
+
+//    {
+//        count++;
+
+//    }
+
+//}
+//Console.WriteLine(count);
+
+
+//Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+//int sum = 0;
+
+//int[] arr = new int[60];
+
+
+
+
+//for (int i = 0; i <arr.Length; i++) 
+//{
+//    if (arr[i] % 2 == 1)
+
+//    {
+//        sum += arr[i];
+
+//    }
+
+//}
+//Console.WriteLine(sum);
+
+
